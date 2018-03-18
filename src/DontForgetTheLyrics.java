1fb119ba6e200da3d1fba25f44cdeb5997646404
League <League@adminisorsimac2.attlocal.net>
2018-03-17 17:27:13 -0700

    Did ChangeCalculator, DontForgetTheLyrics, FruitQuiz

--- src/DontForgetTheLyrics.java
+++ src/DontForgetTheLyrics.java
@@ -20,17 +20,29 @@ public class DontForgetTheLyrics {
 	
 	public static void main(String[] args) {
 		// 4. Make a pop-up to explain the game.
-
+JOptionPane.showMessageDialog(null, "For this game, I'll play the start of a song, and you will guess the rest of the lyrics");
 		// 5. Use the playSound method to play your song.
-
+playSound("RickAstly.wav");
 		// 6. Make a pop-up for the player to type their answer.
-
+String answer = JOptionPane.showInputDialog("What is the next line?");
 		// 7. If they answered correctly, tell them that they were right.
-
+if (answer.equals("run around and desert you")){
+		JOptionPane.showMessageDialog(null, "Correct!");
+}
 		// 8. Otherwise, tell them they are wrong, and give them the answer.
-
+else {
+	JOptionPane.showMessageDialog(null, "Wrong!");
+}
 		// 9. Record another sound and repeat steps 5-8.
+playSound("africa.wav");
+String answer2 = JOptionPane.showInputDialog("What is the next line?");
 
+if (answer2.equals("Africa")){
+	JOptionPane.showMessageDialog(null, "Correct!");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Wrong!");
+}
 		// 10. [optional] Add a points variable that will calculate their final score.
 	}
 
